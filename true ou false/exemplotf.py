ab409eed4c65405986af345eb3b24c8514abc891
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-21 09:10:05 -0300

    true e false

---
+++ true ou false/exemplotf.py
@@ -0,0 +1,46 @@
+x = 10
+y = 10
+
+#maior que 
+print(x>y)
+
+#menor que
+print(y<x)
+
+#maior ou igual
+print(y>=x)
+
+#menor ou igual
+print(x<=y)
+
+
+x = 5
+y = 10
+
+#and
+print(x < 10 and y > 5)
+print(x > 10 and y < 5)
+
+#or
+print(x < 10 or y > 5)
+print(x > 10 or y < 5)
+
+x = 5
+y = 10
+
+#not
+print(not(x < 10))
+print(not(x > 10))
+
+idade = 20
+
+#maior de idade verificação
+idade >= 18
+
+#menor de idade
+idade < 18
+not idade >= 18
+
+
+
+ 
