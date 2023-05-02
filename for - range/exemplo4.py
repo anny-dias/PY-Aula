ef778d5113089241ed378b57390f0817fd3616d8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-02 09:48:07 -0300

    py

--- for - range/exemplo4.py
+++ for - range/exemplo4.py
@@ -1,5 +1,5 @@
 #CONTINUE
-#interrompe a iteração atual o look e passa para proxima
+# #interrompe a iteração atual o look e passa para proxima
 
 for n in range(1, 50):
     if n % 10 == 0:
--- função/exercicio4.py
+++ função/exercicio4.py
@@ -1,1 +1,11 @@
-#Faça uma funçãoque recebeum número inteiro por parâmetro e retorna Truese for par e Falsese for ímpar.
+#Faça uma função que recebe um número inteiro por parâmetro e retorna True se
+# for par e False se for ímpar.
+
+def par_impar(numero):
+    if numero % 2 == 0:
+        print("True")
+    else:
+        print("False")
+
+numero = int(input("Insira um número: "))
+par_impar(numero)
